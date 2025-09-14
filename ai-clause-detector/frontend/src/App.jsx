ea3257ec9f4c7050560e7c619a613875e83f338f
Asmith Mahendrakar <mahendrakarasmith@gmail.com>
2025-09-14 13:17:16 +0530

    Update App.jsx

--- ai-clause-detector/frontend/src/App.jsx
+++ ai-clause-detector/frontend/src/App.jsx
@@ -2,7 +2,9 @@ import Page from './pages/page.jsx'
 
 export default function App() {
   return <Page />
-  
+  <div>
+    <h1>ClauseIQ - DEPLOYMENT TEST - It Works!</h1>
+  <div>
 }
 console.log('API URL being used:', import.meta.env.VITE_API_URL);
 console.log("VITE_API_URL from App.jsx:", import.meta.env.VITE_API_URL);
